9def255a1a970aa8ec9d4ff4e7716d577fa3ea98
Vee <avhadpriyanka88@gmail.com>
2021-11-14 21:44:47 +0530

    search bar on SD admin

--- frontend/src/admin/Pages/studentData/studentData.js
+++ frontend/src/admin/Pages/studentData/studentData.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './styles.scss';
 import Sidebar from '../../Components/Sidebar'
 import Navbar from '../../Components/Navbar';
@@ -6,35 +6,45 @@ import CustomButton from '../../Components/custom-button/custom-button-component
 import data from './data.json';
 
 function StudentData() {
+  const [searchTerm,setSearchTerm] = useState('')
   return (
     <div className='StudentData'>
       <Navbar />
       <div style = {{width:'25%', position:'fixed',zIndex:'1'}}>
-		<Sidebar/>
-		</div>
-		<div style = {{paddingLeft:'30%', paddingTop: '2%',paddingRight: '4%'}}>
-			<div>
-        <h1><b>Student Data</b></h1>
-        <div className = 'row'>
-              <div className = 'col1'><b>Name</b></div>
-              <div className = 'col2'><b>Organisation</b></div>
-              <div className = 'col3'><b>Registered on</b></div>
-              <div className = 'col4'></div>
-        </div>
-        <hr/>
-        {data.map((item,id) => {
+        <Sidebar/>
+      </div>
+		  <div style = {{paddingLeft:'30%', paddingTop: '2%',paddingRight: '4%'}}>
+        <div>
+          <h1><b>Student Data</b></h1>
+
+          <div className = 'row'>
+                <div className = 'col1'><b>Name</b></div>
+                <div className = 'col2'><b>Organisation</b></div>
+                <div className = 'col3'><b>Registered on</b></div>
+                <div className = 'col4'><input type="text" placeholder="Seach Name" onChange={e=>setSearchTerm(e.target.value)} /></div>
+          </div>
+          <hr/>
+        
+          {data.filter((val)=>{
+            if(searchTerm == ""){
+              return val
+            }
+            else if(val.firstname.toLowerCase().includes(searchTerm.toLowerCase())||val.lastname.toLowerCase().includes(searchTerm.toLowerCase())){
+              return val;
+            }
+          }).map((val,id)=>{
             return (
               <div className = 'row'>
-              <div className = 'col1'>{item.firstname} {item.lastname}</div>
-              <div className = 'col2'>{item.organisation}</div>
-              <div className = 'col3'>{item.subscribedOn}</div>
+              <div className = 'col1'>{val.firstname} {val.lastname}</div>
+              <div className = 'col2'>{val.organisation}</div>
+              <div className = 'col3'>{val.subscribedOn}</div>
               <div className = 'col4'><CustomButton type='submit'> Details </CustomButton></div>
               <br/>
               </div>
             );
-        })}
-      </div>
-		</div>
+          })}
+        </div>
+		  </div>
     </div>
   );
 }
