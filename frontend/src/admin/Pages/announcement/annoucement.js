e7bb4165dc0c10dfe44b319c4e3ad5cbed577744
vedanti <vedantikshirsagar22@gmail.com>
2021-11-14 22:33:43 +0530

    announcement partially done

--- frontend/src/admin/Pages/announcement/annoucement.js
+++ frontend/src/admin/Pages/announcement/annoucement.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './styles.scss';
+import { Input } from 'semantic-ui-react';
 import Sidebar from '../../Components/Sidebar'
 import Navbar from '../../Components/Navbar'
+import CustomButton from '../../../Register/components/custom-button/custom-button-component';
 
 function Announcement() {
   return (
@@ -12,6 +14,22 @@ function Announcement() {
 		</div>
 		<div style = {{paddingLeft:'30%', paddingTop: '2%'}}>
 			<h1>Announcements</h1>
+      <div class="post">
+      <form>
+      <label>
+      Title : <br/>
+      <Input type="text" name="title" style={{ width: "800px" }}/>
+      </label><br/>
+      <label>
+      Post : <br/>
+      <Input type="text" name="post" style={{ width: "800px" }}/>
+      </label>
+      <CustomButton type='submit' style={{float:"right"}}>POST</CustomButton>
+      </form>
+      </div>
+      <div class='log'>
+      <h2>Logs</h2>
+      </div>
 		</div>
     </div>
   );
