b9e2175eef33c95b89e23c1f2edfe1e03ce2d701
Vee <avhadpriyanka88@gmail.com>
2021-11-14 20:34:18 +0530

    SD

--- frontend/src/admin/Pages/studentData/studentData.js
+++ frontend/src/admin/Pages/studentData/studentData.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './styles.scss';
 import Sidebar from '../../Components/Sidebar'
-import Navbar from '../../Components/Navbar'
+import Navbar from '../../Components/Navbar';
+import data from './data.json';
 
 function StudentData() {
   return (
@@ -10,8 +11,21 @@ function StudentData() {
       <div style = {{width:'25%', position:'fixed',zIndex:'1',overflow:'auto'}}>
 		<Sidebar/>
 		</div>
-		<div style = {{paddingLeft:'30%', paddingTop: '2%'}}>
-			<h1>Student Data</h1>
+		<div style = {{paddingLeft:'30%', paddingTop: '2%',paddingRight: '4%'}}>
+			<div>
+        <h1>Student Data</h1>
+        <hr/>
+        {data.map((item,id) => {
+            return (
+              <div className = 'row'>
+              <div className = 'col1'>{item.firstname} {item.lastname}</div>
+              <div className = 'col2'>{item.organisation}</div>
+              <div className = 'col3'>{item.subscribedOn}</div>
+              <div className = 'col4'>{item.phone}</div>
+              </div>
+            );
+        })}
+      </div>
 		</div>
     </div>
   );
