90b1ec43210e11b9401453653b1cb4bfb4e3acd3
vedanti <vedantikshirsagar22@gmail.com>
2021-11-16 01:11:46 +0530

    aanouncements/mass reg done

--- frontend/src/admin/Pages/announcement/annoucement.js
+++ frontend/src/admin/Pages/announcement/annoucement.js
@@ -4,7 +4,7 @@ import { Input } from 'semantic-ui-react';
 import Sidebar from '../../Components/Sidebar'
 import Navbar from '../../Components/Navbar'
 import CustomButton from '../../../Register/components/custom-button/custom-button-component';
-
+import data from './announcements.json'
 
 
 function Announcement() {
@@ -12,31 +12,38 @@ function Announcement() {
   const [post, setpost] = useState('');
   return (
     <>
-    <div>
-      <Navbar />
-      <div style = {{width:'25%', position:'fixed',zIndex:'1',overflow:'auto'}}>
-		<Sidebar/>
-		</div>
-		<div style = {{paddingLeft:'30%', paddingTop: '2%'}}>
-			<h1>Announcements</h1>
-      <div class="post">
-      <form>
-      <label>
-      Title : <br/>
-      <Input type="text" name="title" value={title} onChange={(e)=>{settitle(e.target.value)}} style={{ width: "800px" }}/>
-      </label><br/>
-      <label>
-      Post : <br/>
-      <Input type="text" name="post" value={post} onChange={(e)=>{setpost(e.target.value)}} style={{ width: "800px" }}/>
-      </label>
-      <CustomButton type='submit' style={{float:"right"}}>POST</CustomButton>
-      </form>
+      <div>
+        <Navbar />
+        <div style = {{width:'25%', position:'fixed',zIndex:'1',overflow:'auto'}}>
+      <Sidebar/>
       </div>
-      <div class='log'>
-      <h2>Logs</h2>
+      <div style = {{paddingLeft:'30%', paddingTop: '2%'}}>
+        <h1>Announcements</h1>
+        <div class="post">
+          <form>
+            <label class="input-text">
+              Title : <br/>
+              <input type="text" name="title" value={title} onChange={(e)=>{settitle(e.target.value)}} style={{height:"30px" }}/>
+            </label><br/>
+            <label class="input-text">
+              Post : <br/>
+              <input type="text" name="post" value={post} onChange={(e)=>{setpost(e.target.value)}} style={{ height:"150px"}}/>
+            </label>
+            <CustomButton type='submit' style={{float:"right"}}>POST</CustomButton>
+          </form>
+        </div>
+        <div class='log'>
+          <h2>Logs</h2>
+            <div>
+            {data.map((item,i)=>(
+              <span key={i}>
+              <p class="logs"><b>{item.title}</b>: {item.post}</p>
+              </span>
+            ))}
+            </div>
+        </div>
       </div>
-		</div>
-    </div>
+      </div>
     </>
   );
 }
--- frontend/src/admin/Pages/massreg/massreg.js
+++ frontend/src/admin/Pages/massreg/massreg.js
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles.scss';
-import Sidebar from '../../Components/Sidebar'
-import Navbar from '../../Components/Navbar'
+import Sidebar from '../../Components/Sidebar';
+import Navbar from '../../Components/Navbar';
+import * as XLSX from 'xlsx';
+import DataTable from 'react-data-table-component';
 
 function MassRegistration() {
+  const [columns, setColumns] = useState([]);
+  const [data, setData] = useState([]);
+ 
+
+  const processData = dataString => {
+    const dataStringLines = dataString.split(/\r\n|\n/);
+    const headers = dataStringLines[0].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+ 
+    const list = [];
+    for (let i = 1; i < dataStringLines.length; i++) {
+      const row = dataStringLines[i].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+      if (headers && row.length == headers.length) {
+        const obj = {};
+        for (let j = 0; j < headers.length; j++) {
+          let d = row[j];
+          if (d.length > 0) {
+            if (d[0] == '"')
+              d = d.substring(1, d.length - 1);
+            if (d[d.length - 1] == '"')
+              d = d.substring(d.length - 2, 1);
+          }
+          if (headers[j]) {
+            obj[headers[j]] = d;
+          }
+        }
+
+        if (Object.values(obj).filter(x => x).length > 0) {
+          list.push(obj);
+        }
+      }
+    }
+ 
+    const columns = headers.map(c => ({
+      name: c,
+      selector: c,
+    }));
+ 
+    setData(list);
+    setColumns(columns);
+  }
+  const handleFileUpload = e => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (evt) => {
+      const bstr = evt.target.result;
+      const wb = XLSX.read(bstr, { type: 'binary' });
+      const wsname = wb.SheetNames[0];
+      const ws = wb.Sheets[wsname]
+      const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
+      processData(data);
+    };
+    reader.readAsBinaryString(file);
+  }
   return (
     <div>
       <Navbar />
@@ -12,6 +67,21 @@ function MassRegistration() {
 		</div>
 		<div style = {{paddingLeft:'30%', paddingTop: '2%'}}>
 			<h1>Mass Registration</h1>
+      <label class="custom-file-upload">
+          <input
+            type="file"
+            accept=".csv,.xlsx,.xls"
+            onChange={handleFileUpload}
+          />
+          Custom Upload
+      </label>
+      
+      <DataTable
+        pagination
+        highlightOnHover
+        columns={columns}
+        data={data}
+      />
 		</div>
     </div>
   );
