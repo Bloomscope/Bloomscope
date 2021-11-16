811af9ed9cbd53c50543f9d07461646b53c764f5
vedanti <vedantikshirsagar22@gmail.com>
2021-11-16 23:19:59 +0530

    test creation done

--- frontend/src/admin/Pages/TestCreation/TestCreation.js
+++ frontend/src/admin/Pages/TestCreation/TestCreation.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './styles.scss';
 import Dropdown from 'react-dropdown'
 import 'react-dropdown/style.css'
 import Sidebar from '../../Components/Sidebar'
 import Navbar from '../../Components/Navbar'
+import Parameter from '../../Components/parameters/parameter';
+import TabButton from '../../../Register/components/TabButton/TabButton';
+import {FaPlus} from "react-icons/fa";
 
 function TestCreation() {
+  const [openbutton, setopenbutton]=useState(false);
   const options = [
   '1', '2', '3', '4', '5', '6','7','8','9', '10','11','12','13','14','15','16',
   '17','18','19','20','21','22','23','24','25','26','27','28','29','30','31'
@@ -26,7 +30,13 @@ function TestCreation() {
           <span>End Day Number
           <Dropdown options={options}  value={defaultOption} placeholder="Select an option" style={{width:"20px"}} />
           </span>
-          <button>Add Parameter</button>
+          <div>
+            <TabButton onClick={() => setopenbutton(!openbutton)}>
+              <FaPlus className="FaUserGraduate" />
+              Add Parameter
+            </TabButton>
+            <Parameter view={openbutton}/>
+          </div>
         </div>
       </div>
     </div>
