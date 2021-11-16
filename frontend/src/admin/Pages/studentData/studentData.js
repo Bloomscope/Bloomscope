666b96dac994146efbfe5d033c945a78359c517b
Vee <avhadpriyanka88@gmail.com>
2021-11-16 08:54:11 +0530

    popup done

--- frontend/src/admin/Pages/studentData/studentData.js
+++ frontend/src/admin/Pages/studentData/studentData.js
@@ -2,11 +2,50 @@ import React,{useState} from 'react';
 import './styles.scss';
 import Sidebar from '../../Components/Sidebar'
 import Navbar from '../../Components/Navbar';
-import CustomButton from '../../Components/custom-button/custom-button-component';
+import Popup from 'reactjs-popup';
 import data from './data.json';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+
 
 function StudentData() {
+
+  const dataa = [
+    {
+      name: 'P1',
+      t1: 40,
+      t2: 24,
+    },
+    {
+      name: 'P2',
+      t1: 30,
+      t2: 13,
+    },
+    {
+      name: 'P3',
+      t1: 20,
+      t2: 28,
+    },
+    {
+      name: 'P4',
+      t1: 27,
+      t2: 39,
+    },
+    {
+      name: 'P5',
+      t1: 18,
+      t2: 48,
+    },
+    {
+      name: 'P6',
+      t1: 23,
+      t2: 38,
+    },
+  ];
+
+  const [suggestion, setsuggestion] = useState('');
   const [searchTerm,setSearchTerm] = useState('')
+
   return (
     <div className='StudentData'>
       <Navbar />
@@ -18,13 +57,14 @@ function StudentData() {
           <h1><b>Student Data</b></h1>
 
           <div className = 'row'>
-                <div className = 'col1'><b>Name</b></div>
-                <div className = 'col2'><b>Organisation</b></div>
-                <div className = 'col3'><b>Registered on</b></div>
+                <div className = 'col1'><h3>Name</h3></div>
+                <div className = 'col2'><h3>Organisation</h3></div>
+                <div className = 'col3'><h3>Registered on</h3></div>
                 <div className = 'col4'><input type="text" placeholder="Seach Name" onChange={e=>setSearchTerm(e.target.value)} /></div>
           </div>
           <hr/>
-        
+
+          <div style = {{overflowY:'scroll', height:'400px'}}>
           {data.filter((val)=>{
             if(searchTerm == ""){
               return val
@@ -35,14 +75,51 @@ function StudentData() {
           }).map((val,id)=>{
             return (
               <div className = 'row'>
-              <div className = 'col1'>{val.firstname} {val.lastname}</div>
-              <div className = 'col2'>{val.organisation}</div>
-              <div className = 'col3'>{val.subscribedOn}</div>
-              <div className = 'col4'><CustomButton type='submit'> Details </CustomButton></div>
-              <br/>
+                <div className = 'col1' style = {{paddingTop: '7%'}}>{val.firstname} {val.lastname}</div>
+                <div className = 'col2' style = {{paddingTop: '7%'}}>{val.organisation}</div>
+                <div className = 'col3' style = {{paddingTop: '7%'}}>{val.subscribedOn}</div>
+                <div className = 'col4'>
+                  <Popup modal trigger={<button className='custom-button'> Details </button>}>
+                      <div className="popup" style = {{overflowY:'scroll'}}>
+                        Id: {val.id}<br/>
+                        Student name: {val.firstname} {val.lastname} <br/>
+                        Contact: {val.phone}/{val.email}<hr/>
+                        <ResponsiveContainer width="100%" height="60%">
+                          <BarChart
+                            width={500}
+                            height={200}
+                            data={dataa}
+                            margin={{
+                              top: 5,
+                              right: 30,
+                              left: 20,
+                              bottom: 5,
+                            }}
+                          >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Bar dataKey="t1" fill="#8884d8" />
+                            <Bar dataKey="t2" fill="#82ca9d" />
+                          </BarChart>
+                        </ResponsiveContainer>
+                        <form>
+                          <label class="input-text">
+                            Send Suggestion : <br/>
+                            <input type="text" name="post" value={suggestion} onChange={(e)=>{setsuggestion(e.target.value)}} style={{ height:"30px"}}/>
+                          </label>
+                          <button className='custom-button' type='submit' style={{float:"right"}}>Send</button>
+                        </form>
+                      </div>
+                  </Popup>
+                </div>
+                <br/>
               </div>
             );
           })}
+          </div>
         </div>
 		  </div>
     </div>
