21cdda483fa4d6abbc21702dbad1c49ccdc470c2
Vee <avhadpriyanka88@gmail.com>
2021-11-28 07:14:59 +0530

    interactive chart for dashboard done

--- frontend/src/student/Pages/dashboard/dashboard.js
+++ frontend/src/student/Pages/dashboard/dashboard.js
@@ -21,13 +21,16 @@ function Dashboard() {
         <div style={{ padding: "0 0.5rem" }}>
           <Sidebar />
         </div>
-        <div style={{ paddingLeft: "6rem", paddingTop: "1rem" }}>
+        <div style={{ paddingLeft: "6rem", paddingTop: "1rem",width:"70vw" }}>
           <h1>Dashboard</h1>
-          <div style={{backgroundColor:"white",padding:"1rem",width:"35vw",height:"65vh"}}>
+          <div style={{backgroundColor:"white",padding:"1rem",width:"47%",height:"65vh", float:"left"}}>
             <div style={{fontWeight:"bold",fontSize:"1.2rem"}}>Progress Chart</div><br/>
             <div>Each test given is examined on the basis of 6 parameters. These parameters should be worked upon separately. To understand the areas which require work, you can check your progress. This chart analysis displays your journey by comparing your results throughout. You can choose to compare test-wise by only selecting the legends you wish to view and compare.</div><br/>
             <Chart/>
-            </div>
+          </div>
+          <div style={{backgroundColor:"white",padding:"1rem",width:"44%",height:"65vh", float:"right"}}>
+            
+          </div>
         </div>
         </Holder>
     </>
