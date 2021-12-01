19b71c76c7bb6464796f553256936ca17ed79c59
Blazeleo <satviksams@gmail.com>
2021-12-01 09:17:27 +0530

    Basic quiz template

--- frontend/src/student/Pages/test/test copy.js
+++ frontend/src/student/Pages/test/test copy.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import "./styles.scss";
 import Navbar from "../../Components/Navbar";
 import styled from "styled-components";
-import { quiz } from './testData';
-import Quiz from "react-quiz-component";
+import qBank from "./tests.json";
+import { quiz } from 'quiz';
 
 const Questions = styled.div`
   padding: 2rem;
@@ -52,7 +52,32 @@ function Test() {
     <>
       <div>
         <Navbar />
-        <Quiz quiz={quiz}></Quiz>
+        <div>
+          {qBank.map((q, i) => (
+            <div key={i}>
+              <form>
+                <Questions>{q.question}</Questions>
+                <div className="AnswerContainer">
+                  <input type="radio" name={q.question} value={q.A} />
+                  <label htmlFor={q.A}>{q.A}</label>
+                </div>
+                <div className="AnswerContainer">
+                  <input type="radio" name={q.question} value={q.B} />
+                  <label htmlFor={q.B}>{q.B}</label>
+                </div>
+                <div className="AnswerContainer">
+                  <input type="radio" name={q.question} value={q.C} />
+                  <label htmlFor={q.C}>{q.C}</label>
+                </div>
+                <div className="AnswerContainer">
+                  <input type="radio" name={q.question} value={q.D} />
+                  <label htmlFor={q.D}>{q.D}</label>
+                </div>
+              </form>
+              <br/><br/><br/>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
---
+++ frontend/src/student/Pages/test/testData.js
@@ -0,0 +1,30 @@
+
+export const quiz = {
+  "quizTitle": "Demo Quiz",
+  "quizSynopsis": "Good Luck!",
+  "questions": [
+    {
+      "question": "What’s the full name of India’s Prime minister?",
+      "questionType": "text",
+      "answers": [
+        "Narenda Modi",
+        "Singh",
+        "modi",
+        "gandhi"
+      ],
+      "correctAnswer": "1",
+      "point": "1"
+    },
+    {
+      "question": "What is the name of this fruit?",
+      "questionType": "photo",
+      "answers": [
+        "cranberry",
+        "cronberry",
+        "crinberry"
+      ],
+      "correctAnswer": "1",
+      "point": "1"
+    }
+  ]
+}
